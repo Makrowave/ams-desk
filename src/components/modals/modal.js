68b8f45a4ebea6c2417c8d92488da6cd0df0b078
EegleZ <maksstyzej@gmail.com>
2024-10-14 00:16:21 +0200

    --amend

--- src/components/modals/modal.js
+++ src/components/modals/modal.js
@@ -1,8 +1,24 @@
 import { useState } from "react";
 import Image from 'next/image'
 import UseModal from "@/hooks/use_modal";
+
+/**
+ * Modal component which creates a popup with dimmer background.
+ * To use modal you have to use it within ModalContextProvider.
+ * To change modal values use:
+ * setIsOpen - changes boolean to close/open modal;
+ * setModalChildren - sets children for modal;
+ * setTitle - sets title for modal;
+ * To initally set modal content use above functions preferably
+ * in the page or at the modal's parent component 
+ * (make sure it's nested in ModalContextProvider).
+ * On close (pressing title bar's close button)
+ * title is set to empty string and
+ * modalChildren is set to fragment.
+ * @returns ModalComponent
+ */
 export default function Modal() {
-  const {isOpen, setIsOpen, modalChildren, setModalChildren, title, setTitle} = UseModal();
+  const { isOpen, setIsOpen, modalChildren, setModalChildren, title, setTitle } = UseModal();
 
   function handleClose() {
     setIsOpen(false);
@@ -15,21 +31,21 @@ export default function Modal() {
       {
         isOpen && (
           <div className="">
-          <div className="fixed top-0 bottom-0 left-0 right-0 pointer-events-auto transparent-bg z-10">
-            <div className="bg-primary flex flex-col w-fit h-fit absolute top-0 bottom-0 left-0 right-0 m-auto rounded-2xl border-border border-2 min-w-80 min-h-80">
-              {/*Header*/}
-              <div className="flex justify-between bg-secondary rounded-t-2xl px-6 align-center h-10 shadow-md">
-                <h2 className="leading-10">{title}</h2>
-                <button className="flex align-center " onClick={() => handleClose()}>
-                  <img className='h-5 self-center px-1' src='/close.png' /> 
-                </button>
-              </div>
-              {/*Content*/}
-              <div className="mx-6 pt-2">
-                {modalChildren}
+            <div className="fixed top-0 bottom-0 left-0 right-0 pointer-events-auto transparent-bg z-10">
+              <div className="bg-primary flex flex-col w-fit h-fit absolute top-0 bottom-0 left-0 right-0 m-auto rounded-2xl border-border border-2 min-w-80 min-h-80">
+                {/*Header*/}
+                <div className="flex justify-between bg-secondary rounded-t-2xl px-6 align-center h-10 shadow-md">
+                  <h2 className="leading-10">{title}</h2>
+                  <button className="flex align-center " onClick={() => handleClose()}>
+                    <img className='h-5 self-center px-1' src='/close.png' />
+                  </button>
+                </div>
+                {/*Content*/}
+                <div className="mx-6 pt-2">
+                  {modalChildren}
+                </div>
               </div>
             </div>
-          </div>
           </div>
         )
       }
