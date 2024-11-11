57b4d5591304ee3102dd7dfa33a89526873d2045
EegleZ <maksstyzej@gmail.com>
2024-11-12 00:00:03 +0100

    --ammend

--- src/components/modals/delete_modal.js
+++ src/components/modals/delete_modal.js
@@ -2,11 +2,14 @@ import useAxiosAdmin from "@/hooks/use_axios_admin";
 import useAxiosPrivate from "@/hooks/use_axios_private";
 import useModal from "@/hooks/use_modal";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
+import ErrorDisplay from "../error/error_display";
+import { useState } from "react";
 
 export default function DeleteModal({ refetchQueryKey, id, url, admin = false }) {
   const axios = admin ? useAxiosAdmin() : useAxiosPrivate();
   const { setIsOpen } = useModal();
   const queryClient = useQueryClient();
+  const [error, setError] = useState("");
   const mutation = useMutation({
     mutationFn: async () => {
       return await axios.delete(url + id);
@@ -25,6 +28,7 @@ export default function DeleteModal({ refetchQueryKey, id, url, admin = false })
 
   return (
     <div className='modal-basic'>
+      <ErrorDisplay message={error} isVisible={error !== ""} />
       <div className='w-full *:py-2'>
         <h2>
           <b>Czy na pewno?</b>
