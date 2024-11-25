b7317bf61e6ff7038d72c4f9836d6b7810894483
EegleZ <maksstyzej@gmail.com>
2024-11-25 14:16:13 +0100

    feat: new color picker

--- src/components/input/color_input.js
+++ src/components/input/color_input.js
@@ -1,16 +1,29 @@
-export default function ColorInput({ title, value, setValue, className }) {
+import { useEffect, useRef, useState } from "react";
+import { ChromePicker } from "react-color";
+
+export default function ColorInput({ title, value, setValue }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const selectRef = useRef(null);
+
+  function handleClickOutside(event) {
+    if (selectRef.current && !selectRef.current.contains(event.target)) {
+      setIsOpen(false);
+    }
+  }
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
   return (
-    <div className={"flex justify-between " + className}>
+    <div className={"flex justify-between "}>
       <p className='self-center'>{title}</p>
-      <div>
-        <input
-          className='h-10'
-          type='color'
-          value={value}
-          onChange={(e) => {
-            setValue(e.target.value);
-          }}
-        />
+      <div ref={selectRef}>
+        <button className='h-10 w-10' onClick={() => setIsOpen(!isOpen)} style={{ background: value }} />
+        {isOpen && (
+          <ChromePicker className='absolute' color={value} onChange={(e) => setValue(e.hex)} disableAlpha={true} />
+        )}
       </div>
     </div>
   );
