4f8be87ec938aa6742ad0b82b4b96c33167b6205
makrowave <maksstyzej@gmail.com>
2025-02-27 13:11:33 +0100

    feat: validations in addpartmodal

--- src/components/modals/repair/AddPartModal.js
+++ src/components/modals/repair/AddPartModal.js
@@ -2,6 +2,7 @@ import ValidationFetchSelect from "@/components/validation/ValidationFetchSelect
 import { QUERY_KEYS } from "@/util/query_keys";
 import { useState } from "react";
 import { FaCheck, FaXmark } from "react-icons/fa6";
+import { REGEX } from "@/util/regex";
 
 export default function AddPartModal({}) {
   const [category, setCategory] = useState("");
@@ -9,11 +10,27 @@ export default function AddPartModal({}) {
   const [price, setPrice] = useState("");
   const [unit, setUnit] = useState(1);
 
-  const [isNameValid, setIsNameValid] = useState();
-  const [isPriceValid, setIsPriceValid] = useState();
+  const [isNameValid, setIsNameValid] = useState(false);
+  const [isPriceValid, setIsPriceValid] = useState(false);
 
-  const updateName = (value) => {};
-  const updatePrice = (value) => {};
+  const updateName = (value) => {
+    if (REGEX.POLISH_TEXT.test(value) && value.length <= 40) {
+      setIsNameValid(true);
+    } else {
+      setIsNameValid(false);
+    }
+    setName(value);
+  };
+
+  const updatePrice = (value) => {
+    const num = Number(value);
+    if (!isNaN(num) && num > 0 && num < 10000) {
+      setIsPriceValid(true);
+    } else {
+      setIsPriceValid(false);
+    }
+    setPrice(value);
+  };
 
   return (
     <div className='flex flex-col w-full justify-between items-center py-2'>
@@ -71,7 +88,12 @@ export default function AddPartModal({}) {
           title='Jednostka'
           default_title='Wybierz z listy'
         />
-        <div className='button-primary text-center'>Dodaj</div>
+        <button
+          disabled={!(isNameValid && isPriceValid && category !== "" && unit !== "")}
+          className='button-primary text-center disabled:bg-gray-400'
+        >
+          Dodaj
+        </button>
       </div>
     </div>
   );
