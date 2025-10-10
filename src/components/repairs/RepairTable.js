c89827e7ebdd784e3b3313f7e81cbb60e946c7c9
makrowave <maksstyzej@gmail.com>
2025-10-10 17:20:48 +0200

    feat!: New repair table
    Breaks place stored in local storage - wrong filter will be applied on first launch

--- src/components/repairs/RepairTable.js
+++ src/components/repairs/RepairTable.js
@@ -1,27 +1,49 @@
-"use client";
-import {FaPlus} from "react-icons/fa6";
-import RepairRecord from "./RepairRecord";
-import NewRepairModal from "../modals/repair/NewRepairModal";
-import useAxiosPrivate from "@/hooks/useAxiosPrivate";
-import {useQuery} from "@tanstack/react-query";
-import {useState} from "react";
-import {usePlacesNotStorageQuery} from "@/hooks/queryHooks";
-import MaterialModal from "@/components/modals/MaterialModal";
+'use client';
+import { FaChevronRight, FaPlus } from 'react-icons/fa6';
+import NewRepairModal from '../modals/repair/NewRepairModal';
+import useAxiosPrivate from '@/hooks/useAxiosPrivate';
+import { useQuery } from '@tanstack/react-query';
+import { useEffect, useMemo, useState } from 'react';
+import { usePlacesNotStorageQuery } from '@/hooks/queryHooks';
+import MaterialModal from '@/components/modals/MaterialModal';
+import {
+  MaterialReactTable,
+  useMaterialReactTable,
+} from 'material-react-table';
+import { Box, IconButton } from '@mui/material';
+import Link from 'next/link';
+import { formatPhone } from '@/util/formatting';
+import { MRT_Localization_PL } from 'material-react-table/locales/pl';
 
-export default function RepairTable({src, addButton, localKey}) {
+export default function RepairTable({ src, addButton, localKey }) {
   const axiosPrivate = useAxiosPrivate();
   const storageKey = `repairTable.${localKey}`;
+
+  const [filters, setFilters] = useState([
+    { id: 'placeName', value: localStorage.getItem(storageKey) },
+  ]);
+
+  useEffect(() => {
+    const place = filters.find((f) => f.id === 'placeName');
+    if (place) {
+      localStorage.setItem(storageKey, place.value);
+    } else {
+      if (localStorage.getItem(storageKey) !== '') {
+        localStorage.setItem(storageKey, '');
+      }
+    }
+  }, [filters]);
+
   const [place, setPlace] = useState(() => {
-    if (typeof window !== "undefined") {
+    if (typeof window !== 'undefined') {
       const storedValue = localStorage.getItem(storageKey);
       return isNaN(Number(storedValue)) ? 0 : Number(storedValue);
     }
     return 0;
   });
-  const [phone, setPhone] = useState("");
   const getSrc = () => `${src}&place=${place}`;
 
-  const {data, isPending, isError, error} = useQuery({
+  const { data, isPending, isError, error } = useQuery({
     queryKey: [getSrc()],
     queryFn: async () => {
       const response = await axiosPrivate.get(getSrc());
@@ -29,97 +51,90 @@ export default function RepairTable({src, addButton, localKey}) {
     },
   });
 
-  const {data: placesData, isError: isPlacesError, isLoading: isPlacesLoading} = usePlacesNotStorageQuery();
-  const places = createPlaces(!isPlacesLoading && !isPlacesError ? placesData : []);
+  const {
+    data: placesData,
+    isError: isPlacesError,
+    isLoading: isPlacesLoading,
+  } = usePlacesNotStorageQuery();
+  const places = createPlaces(
+    !isPlacesLoading && !isPlacesError ? placesData : [],
+  );
 
-  return (
-    <div>
-      <div className='flex justify-between items-center h-12'>
-        <div className='flex h-full'>
-          {places.map((p) => (
-            <HeaderButton
-              onClick={(id) => {
-                setPlace(id);
-                localStorage.setItem(storageKey, id);
-              }}
-              key={p.placeId}
-              id={p.placeId}
-              selected={place === p.placeId}
-              text={p.placeName}
-            />
-          ))}
-        </div>
-        <div className='flex items-center'>
-          <div className='flex p-2'>
-            <input
-              className='border-border border rounded-lg w-32 text-center'
-              placeholder='Telefon'
-              value={phone}
-              onChange={(e) => setPhone(e.target.value)}
-            />
-          </div>
-          {addButton && (
-            <MaterialModal label={"Nowe zgłoszenie"} button={
-              <button
-                className='flex justify-center items-center hover:bg-gray-400 transition-colors duration-200 rounded-lg w-fit h-fit p-2'>
-                <FaPlus className='w-6 h-6'/>
-              </button>
-            }>
-              <NewRepairModal/>
-            </MaterialModal>
-          )}
-        </div>
-      </div>
-      <div className='text-base max-h-[500px] overflow-y-auto'>
-        <table
-          className={
-            place === 0
-              ? "w-[900px] border-separate border-spacing-0 border rounded-b-lg rounded-tr-lg *:*:*:p-3"
-              : "w-[900px] border-separate border-spacing-0 border rounded-b-lg rounded-t-lg *:*:*:p-3"
+  const columns = useMemo(
+    () => [
+      { accessorKey: 'id', header: 'Nr.' },
+      {
+        accessorKey: 'date',
+        header: 'Data',
+        Cell: ({ row }) =>
+          new Date(row.original.date).toLocaleDateString('pl-PL'),
+      },
+      {
+        accessorKey: 'phoneNumber',
+        header: 'Telefon',
+        Cell: ({ renderedCellValue }) => formatPhone(renderedCellValue),
+      },
+      { accessorKey: 'bikeName', header: 'Rower' },
+      {
+        id: 'status.id', //possibly statusId
+        accessorFn: (row) => row.status?.name ?? '',
+        header: 'Status',
+        Cell: ({ row, renderedCellValue }) => (
+          <Box
+            sx={{
+              p: 1,
+              textAlign: 'center',
+              borderRadius: 2,
+              background: row.original.status?.color,
+            }}
+          >
+            {renderedCellValue}
+          </Box>
+        ),
+      },
+      { accessorKey: 'placeName', header: 'Miejsce' },
+      {
+        id: 'actions',
+        header: '',
+        Cell: ({ row }) => (
+          <IconButton LinkComponent={Link} href={`/serwis/${row.original.id}`}>
+            <FaChevronRight />
+          </IconButton>
+        ),
+      },
+    ],
+    [],
+  );
+
+  const table = useMaterialReactTable({
+    columns: columns,
+    data: data ?? [],
+    initialState: {
+      showColumnFilters: true,
+    },
+    onColumnFiltersChange: setFilters,
+    state: {
+      columnFilters: filters,
+    },
+    localization: MRT_Localization_PL,
+    renderTopToolbarCustomActions: () =>
+      addButton && (
+        <MaterialModal
+          label={'Nowe zgłoszenie'}
+          button={
+            <IconButton>
+              <FaPlus className="w-6 h-6" />
+            </IconButton>
           }
         >
-          <thead>
-          <tr className='*:bg-secondary'>
-            <th>Nr.</th>
-            <th>Data</th>
-            <th className={"min-w-40"}>Telefon</th>
-            <th>Rower</th>
-            <th>Status</th>
-            <th>Miejsce</th>
-            <th className='rounded-tr-lg'></th>
-          </tr>
-          </thead>
-          <tbody className=''>
-          {isPending && <tr>
-            <td colSpan={6} className="text-center">Ładowanie...</td>
-          </tr>}
-          {!isPending && !isError && data.length === 0 && <tr>
-            <td colSpan={6} className="text-center">Brak zgłoszeń</td>
-          </tr>}
-          {!isPending &&
-            !isError &&
-            data
-              .filter((row) => row.phoneNumber.includes(phone))
-              .map((row, index) => <RepairRecord last={index === data.length - 1} repair={row}
-                                                 key={row.id}/>)}
-          </tbody>
-        </table>
-      </div>
-    </div>
-  );
-}
+          <NewRepairModal />
+        </MaterialModal>
+      ),
+  });
 
-function HeaderButton({selected, onClick, id, text}) {
-  return (
-    <button
-      className={selected ? "p-2 w-fit rounded-t-lg h-full bg-secondary" : "p-2 w-fit rounded-t-lg h-full"}
-      onClick={() => onClick(id)}
-    >
-      {text}
-    </button>
-  );
+  return <MaterialReactTable table={table} />;
 }
 
 const createPlaces = (places) => {
-  return [{placeId: 0, placeName: "Wszystkie"}, ...places];
+  return [{ placeId: 0, placeName: 'Wszystkie' }, ...places];
 };
